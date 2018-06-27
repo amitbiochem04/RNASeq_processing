60274dac03c9740e539a45d71a5ac2d1a9fcdbe6
amitbiochem04 <amitbiochem04@gmail.com>
2018-06-27 21:04:25 +0200

    Update bam.sh

--- bam.sh
+++ bam.sh
@@ -5,3 +5,5 @@ bamToBed -i lane1G1_sequence.bam -split > accG1.bed
 awk '{ print $1"\t"$2"\t"$3"\t"$5 }' G2.bed > G2.bedgraph
 ###cpnvert bam to bigwig
 bamCoverage -b lane1G3_sequence.bam -o G3.bigWig
+###sort bedgraph 
+sort -k1,1 -k2,2 unsorted.bedGraph > sorted.bedGraph
