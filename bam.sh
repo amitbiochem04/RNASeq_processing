2570336b600f911028c58ab56d8c96b8c0e7f8ee
amitbiochem04 <amitbiochem04@gmail.com>
2018-06-27 20:05:13 +0200

    Create bam.sh

---
+++ bam.sh
@@ -0,0 +1,7 @@
+
+##bam to bed
+bamToBed -i lane1G1_sequence.bam -split > accG1.bed
+###bed to bed graph
+awk '{ print $1"\t"$2"\t"$3"\t"$5 }' G2.bed > G2.bedgraph
+###cpnvert bam to bigwig
+bamCoverage -b lane1G3_sequence.bam -o G3.bigWig
