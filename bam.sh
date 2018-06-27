6da67863807d1f2355ec3fd461a7c23f99a762c5
amitbiochem04 <amitbiochem04@gmail.com>
2018-06-27 21:06:09 +0200

    Update bam.sh

--- bam.sh
+++ bam.sh
@@ -7,3 +7,5 @@ awk '{ print $1"\t"$2"\t"$3"\t"$5 }' G2.bed > G2.bedgraph
 bamCoverage -b lane1G3_sequence.bam -o G3.bigWig
 ###sort bedgraph 
 sort -k1,1 -k2,2 unsorted.bedGraph > sorted.bedGraph
+####track
+https://davetang.org/muse/2012/03/15/visualising-rna-seq-like-data/
