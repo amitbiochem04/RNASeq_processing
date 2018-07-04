c62b744a5bcd3d83a26056820a8b7b6d8b53b8b2
amitbiochem04 <amitbiochem04@gmail.com>
2018-07-04 09:17:37 +0200

    Update bam.sh

--- bam.sh
+++ bam.sh
@@ -12,3 +12,53 @@ https://davetang.org/muse/2012/03/15/visualising-rna-seq-like-data/
 ###split bw in forward and reverse was 
 bamCoverage -p 20 --filterRNAstrand forward -b lane1G3_sequence.bam -o G3_f.m.bw
 bamCoverage -p 20 --filterRNAstrand reverse -b lane1G3_sequence.bam -o G3_r.m.bw
+###########gtf processing 
+
+cat Mus_musculus.GRCm38.92.gtf | grep -v "^#" | cut -f3 | sort | uniq -c | sort -k1rn
+801623 exon
+ 507246 CDS
+ 135276 transcript
+  90940 five_prime_utr
+  81886 three_prime_utr
+  57560 start_codon
+  53801 gene
+  53487 stop_codon
+     65 Selenocysteine
+  #####
+  
+cat Mus_musculus.GRCm38.92.gtf |
+awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4-1,$5}' |
+~/softwear/bedtools2/bin/sortBed |
+~/softwear/bedtools2/bin/mergeBed -i - | gzip > gencode_v${v}_exon_merged.bed.gz
+
+
+
+######chromosme size check from fasta file
+samtools faidx genome.fa
+cut -f1,2 genome.fa.fai > genome.size
+
+### other way 
+pip install pyfaidx
+faidx -i chromsizes input.fa > output.chromsizes
+
+#####separte coding and noncoding 
+
+cat Mus_musculus.GRCm38.92.gtf |
+awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4-1,$5}' |
+~/softwear/bedtools2/bin/sortBed |
+~/softwear/bedtools2/bin/mergeBed -i - | gzip > gencode_v${v}_exon_merged.bed.gz
+
+
+cat Mus_musculus.GRCm38.92.gtf |
+awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' |
+~/softwear/bedtools2/bin/sortBed |
+~/softwear/bedtools2/bin/subtractBed -a stdin -b gencode_v${v}_exon_merged.bed.gz |
+gzip > gencode_v${v}_intron.bed.gz
+ 
+#let's intersect the two files
+#this shouldn't produce any output
+intersectBed -a gencode_v${v}_exon_merged.bed.gz -b gencode_v${v}_intron.bed.gz
+
+
+
+
